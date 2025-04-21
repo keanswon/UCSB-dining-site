f2c7afc163d36cbf9110e03d3b351031caae1980
keanswon <seankwon06@gmail.com>
2025-04-21 14:50:05 -0700

    Updated ReADME, fixed API base URl

---
+++ database/commonsapi.py
@@ -0,0 +1,39 @@
+# file that works with the UCSB dining commons API to get hours, etc.
+
+import os
+import requests
+from dotenv import load_dotenv, find_dotenv
+from datetime import date
+import time
+
+load_dotenv(find_dotenv())
+
+# 1) Configuration
+API_BASE = "https://api.ucsb.edu/dining/commons/v1"
+API_KEY  =  os.getenv("API_KEY")
+print(API_KEY)
+
+headers = {
+    "ucsb-api-key": API_KEY
+}
+
+resp = requests.get(f"{API_BASE}/",       headers=headers)
+resp.raise_for_status()
+info = resp.json()
+print("Dining Commons Info:", info)
+
+today = date.today().isoformat()
+resp = requests.get(
+    f"{API_BASE}/meals/names",
+    headers=headers,
+    params={"date": today}
+)
+resp.raise_for_status()
+meals = resp.json()
+print(f"Meals on {today}:", meals)
+
+today = time.strftime("%Y-%m-%d", time.localtime())
+resp = requests.get(f"{API_BASE}/hours/{today}", headers=headers)
+resp.raise_for_status()
+hours = resp.json()
+print("Service Hours:", hours)
